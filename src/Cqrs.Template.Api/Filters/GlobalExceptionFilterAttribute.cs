2db6c305dfe249bc1408e4b5e621c68c55f7b2cb
Gabriel Guedes <gabrielgdc15@gmail.com>
2022-03-03 13:37:54 -0300

    add .editorconfig file

--- src/Cqrs.Template.Api/Filters/GlobalExceptionFilterAttribute.cs
+++ src/Cqrs.Template.Api/Filters/GlobalExceptionFilterAttribute.cs
@@ -2,26 +2,36 @@ using System;
 using Cqrs.Template.Api.Filters.ErrorsModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 
 namespace Cqrs.Template.Api.Filters;
 
 public class GlobalExceptionFilterAttribute : Attribute, IExceptionFilter
 {
-    public GlobalExceptionFilterAttribute()
+    private ILogger<GlobalExceptionFilterAttribute> _logger;
+
+    public GlobalExceptionFilterAttribute(ILogger<GlobalExceptionFilterAttribute> logger)
     {
+        _logger = logger;
     }
 
     public void OnException(ExceptionContext context)
     {
-        context.Result = new BadRequestObjectResult(
-            new DefaultError(false,
-                new[]
-                {
-                    new ErrorsResponse(Environment.GetEnvironmentVariable("GlobalErrorCode"),
-                        Environment.GetEnvironmentVariable("GlobalErrorMessage"),
-                        DateTime.Now)
-                }
-            )
+        var eventId = new EventId(188, "GlobalException");
+
+        _logger.LogError(eventId, context.Exception, context.Exception.Message);
+
+        var errorMessage = new DefaultError(false,
+            new[]
+            {
+                new ErrorsResponse(
+                    Environment.GetEnvironmentVariable("GlobalErrorCode"),
+                    Environment.GetEnvironmentVariable("GlobalErrorMessage"),
+                    DateTime.Now
+                )
+            }
         );
+
+        context.Result = new BadRequestObjectResult(errorMessage);
     }
 }
